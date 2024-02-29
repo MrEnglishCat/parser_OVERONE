6ad0a2a075ef7ec093676b086f7a4db6bd9410ce
Memris <mr.englishcat@gmail.com>
2024-02-29 13:11:12 +0300

    Initial commit

---
+++ project_1/app_1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def url1(request):
+    return HttpResponse("Ответ 1")
+
+
+def url2(request):
+    return HttpResponse("Ответ 2")
+
+
+def url3(request):
+    return HttpResponse(request)
