5dd0a866164de958a0f56e6fc4cab780ae779469
mreng <mr.englishcat@gmail.com>
2024-03-06 16:24:08 +0300

    add text area/ item_admin

--- project_1/project_1/settings.py
+++ project_1/project_1/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)roevg8m0x5ms-pkqn_8dt@in4%-mbz0s(%9ed=ho4s7dak@kx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
