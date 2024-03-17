79080115f347caa5170a8bb4081c6659b3b245f0
Memris <mr.englishcat@gmail.com>
2024-03-17 09:32:16 +0300

    update serializers.py - GetAllDataSerializer

--- project_1/app_1/serializers.py
+++ project_1/app_1/serializers.py
@@ -12,13 +12,14 @@ class GetAllDataSerializer(serializers.Serializer):
     get_offset_tz = datetime.now().astimezone().strftime("%z")
 
     id = serializers.PrimaryKeyRelatedField(read_only=True)
-    link = serializers.CharField()
+    link = serializers.CharField(read_only=True)
     price = serializers.DecimalField(
+        read_only=True,
         max_digits=10,
         decimal_places=4,
         min_value=0
     )
-    description = serializers.CharField()
+    description = serializers.CharField(read_only=True)
     update_datetime = serializers.DateTimeField(read_only=True, format=f'%Y-%m-%d %H:%M:%S {get_offset_tz}UTC [{get_name_timezone}]')
     parse_datetime = serializers.DateTimeField(read_only=True, format=f'%Y-%m-%d %H:%M:%S {get_offset_tz}UTC [{get_name_timezone}]', input_formats=["%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d"])
 
